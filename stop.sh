0ceeeb4a3e180e916fd3f1fa4fdbda7b4fde9826
Notaphish <andrew@meiklewise.com>
2020-12-26 18:24:20 +0000

    Add ruuvitag, refactor to use same influx and grafana

--- stop.sh
+++ stop.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f speedtest-grafana/docker-compose.yml down
+docker-compose -f docker-compose.yml -f speedtest-grafana/docker-compose.yml -f ruuvitag/docker-compose.yml down
