9442c2d5c1abfcb330dacf61ce550f7c47a2e442
Notaphish <andrew@meiklewise.com>
2020-12-31 00:26:02 +0000

    Specify full path for docker-compose ( for CRON )

--- dc.sh
+++ dc.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f speedtest-grafana/docker-compose.yml -f ruuvitag/docker-compose.yml $@
+/usr/local/bin/docker-compose -f docker-compose.yml -f speedtest-grafana/docker-compose.yml -f ruuvitag/docker-compose.yml $@
