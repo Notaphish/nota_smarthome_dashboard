daad093d14498d5ef8577e7c0d40f1977c67eac4
Notaphish <andrew@meiklewise.com>
2021-01-25 00:16:34 +0000

    Incorporate lat/long changes

--- weather/app.js
+++ weather/app.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const fetchWeatherData = require("./lib/weather");
 const writeToInfluxdb = require("./lib/influx");
 
-const {CITY_NAME} = process.env;
+const {CITY_NAME, LAT, LONG} = process.env;
 
 ( async () => {
-    const weatherData = await fetchWeatherData(CITY_NAME);
-    await writeToInfluxdb(weatherData)
+    const weatherData = await fetchWeatherData({cityName: CITY_NAME, lat: LAT, long: LONG});
+    writeToInfluxdb(weatherData)
 })();
